8ab07c125c1e3c722a7d57a69f397101330d3fe8
kys980819 <kys980819@gmail.com>
2025-10-14 17:41:49 +0900

    라이트모드 로고 수정

--- assets/assets.js
+++ assets/assets.js
@@ -1,4 +1,3 @@
-import user_image from './user-image.png';
 import code_icon from './code-icon.png';
 import code_icon_dark from './code-icon-dark.png';
 import edu_icon from './edu-icon.png';
@@ -14,7 +13,6 @@ import logo from './logo.png';
 import logo_dark from './logo_dark.png';
 import mail_icon from './mail_icon.png';
 import mail_icon_dark from './mail_icon_dark.png';
-import profile_img from './profile-img.png';
 import download_icon from './download-icon.png';
 import hand_icon from './hand-icon.png';
 import header_bg_color from './header-bg-color.png';
@@ -37,7 +35,6 @@ import right_arrow_bold_dark from './right-arrow-bold-dark.png';
 import profile_kys_img from './profile_kys_img.png';
 
 export const assets = {
-    user_image,
     code_icon,
     code_icon_dark,
     edu_icon,
@@ -53,7 +50,6 @@ export const assets = {
     logo_dark,
     mail_icon,
     mail_icon_dark,
-    profile_img,
     download_icon,
     hand_icon,
     header_bg_color,
